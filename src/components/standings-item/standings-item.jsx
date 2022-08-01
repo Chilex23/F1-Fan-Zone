84bd7af45861317b64fe877454fc2b6ba1d39893
Chilex23 <onumaegbuchilex@gmail.com>
2022-08-01 23:16:11 +0100

    Type: fix, changed the api endpoint for fetching data

--- src/components/standings-item/standings-item.jsx
+++ src/components/standings-item/standings-item.jsx
@@ -1,18 +1,21 @@
 import React from "react";
-import findDriver from "../../utils/find-driver";
 
 const StandingsItem = ({ driver }) => {
-    const { POS, Name, Car, PTS } = driver;
+    const { position, 
+        points,
+        Driver: { familyName, givenName },
+        Constructors: { name }, 
+    } = driver; 
     return (
         <div className="group w-[80%] mt-1 mx-auto bg-white px-4 py-3 hover:bg-gray-800 hover:text-white rounded-lg flex justify-between relative md:top-10 lg:top-32">
             <div className="flex gap-3 items-center">
-                <span className="font-extrabold">{POS}</span>
-                <span className="text-lg">{Name} <span className="text-xl font-bold uppercase">{findDriver(Name).familyName }</span></span>
-                <span className="text-gray-500 text-md">{Car}</span>
+                <span className="font-extrabold">{position}</span>
+                <span className="text-lg">{givenName} <span className="text-xl font-bold uppercase">{familyName }</span></span>
+                <span className="text-gray-500 text-md">{name}</span>
             </div>
 
             <div>
-                <span className="bg-gray-300 py-1 px-3 rounded-xl group-hover:text-black">{PTS} PTS</span>
+                <span className="bg-gray-300 py-1 px-3 rounded-xl group-hover:text-black">{points} PTS</span>
             </div>
         </div>
     );
--- src/components/table-row/table-row.jsx
+++ src/components/table-row/table-row.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const TableRow = ({ driver }) => {
-    const { position, points, Driver: { familyName, givenName }, Constructors: { name }, nationality } = driver; 
+    const { position, 
+            points,
+            Driver: { familyName, givenName },
+            Constructors: { name }, 
+            nationality 
+        } = driver; 
     return (
         <tr>
             <td>{ position }</td>
